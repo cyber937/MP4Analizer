717b6a50f9dd781c343bdc02e5955be8db0d09f4
Kiyoshi Nagahama <kiyoshi@cyberseeds.com>
2024-07-08 08:58:50 -0600

    Initial Commit

---
+++ MP4Analizer/MP4AnalizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MP4AnalizerApp.swift
+//  MP4Analizer
+//
+//  Created by Kiyoshi Nagahama on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct MP4AnalizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
